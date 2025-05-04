2b4b35e3e41fef5fa5e4c74f40aa4b127e92ccb0
Idehen Divine <idehendivine16@gmail.com>
2025-05-04 05:17:24 +0100

    Refactor home component to use Angular components for layout and remove unused navbar functionality

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,32 +1,21 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NavComponent } from '../../components/homepage/nav/nav.component';
+import { HeroComponent } from '../../components/homepage/hero/hero.component';
+import { ServiceComponent } from '../../components/homepage/service/service.component';
+import { DesignComponent } from '../../components/homepage/design/design.component';
+import { GalleryComponent } from '../../components/homepage/gallery/gallery.component';
+import { FooterComponent } from '../../components/homepage/footer/footer.component';
+import { TestimonialComponent } from '../../components/homepage/testimonial/testimonial.component';
 
 @Component({
   selector: 'app-home',
+  imports: [NavComponent, HeroComponent, ServiceComponent, DesignComponent, TestimonialComponent, GalleryComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('nav') nav!: ElementRef;
-  @ViewChild('swapOff') swapOff!: ElementRef;
-  @ViewChild('swapOn') swapOn!: ElementRef;
-
-  isMenuOpen = false;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen;
-    this.updateIconState();
-  }
-
-  private updateIconState(): void {
-    if (this.swapOff && this.swapOn) {
-      this.swapOff.nativeElement.classList.toggle('hidden', this.isMenuOpen);
-      this.swapOn.nativeElement.classList.toggle('hidden', !this.isMenuOpen);
-      this.swapOff.nativeElement.classList.toggle('show', !this.isMenuOpen);
-    }
-  }
+  ngOnInit() {}
 }
